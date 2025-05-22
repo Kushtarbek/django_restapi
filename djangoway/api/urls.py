7109fb36d5420096792ef1dde9d265d7088973e5
kushtarbek <dr.kushtarbek@gmail.com>
2025-05-22 14:56:11 -0700

    wip: updated paths

--- djangoway/api/urls.py
+++ djangoway/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import get_user
+from .views import get_users, create_users, user_detail
 
 urlpatterns = [
-    path('users/', get_user, name='get_user'),
+    path('users/', get_users, name='get_users'),
+    path('users/create', create_users, name='create_user'),
+    path('users/<int:pk>', user_detail, name='user_detail'),
 ]
