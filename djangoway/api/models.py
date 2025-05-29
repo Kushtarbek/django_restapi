de5dea59dd4b96327bc69ec739dc74010f722468
kushtarbek <dr.kushtarbek@gmail.com>
2025-05-29 14:07:02 -0700

    updated urls

--- djangoway/api/models.py
+++ djangoway/api/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class User(models.Model):
     age = models.IntegerField()
     name = models.CharField(max_length=100)
+    occupation = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
