f0a3fce20bcd5b8b0a6c363a5067952b9828e49a
kushtarbek <dr.kushtarbek@gmail.com>
2025-05-29 14:06:29 -0700

    added paginated data table

--- djangoway/frontend/myreactapp/src/App.js
+++ djangoway/frontend/myreactapp/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import DataTable from "./components/DataTable";
 
 function App() {
   return (
     <div className="App">
       <h2>Frontend is loaded!</h2>
+      <DataTable />
     </div>
   );
 }
